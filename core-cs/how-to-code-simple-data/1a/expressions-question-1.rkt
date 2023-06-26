c19b0d13c38f66cfd3960c4a8f2a7f9ec8ab29ab
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-06-26 11:47:39 -0300

    docs(htc-simple-data/1a): solution expressions question 1

---
+++ core-cs/how-to-code-simple-data/1a/expressions-question-1.rkt
@@ -0,0 +1,7 @@
+; PROBLEM:
+; 
+; Assume that the two short of a right triangle have length 3 and 4. What is the length of the long side?
+; Write a BSL expression that produces the value of ? for this triangle where the other
+; two sides have lengths 3 and 4.
+
+(sqrt (+ (sqr 3)(sqr 4)))
