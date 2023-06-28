181197840cc809b2d587c6bac3f574c8de91fc55
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-06-28 08:29:20 -0300

    docs(htc-simple-data/1a): solution bsl-problem 5

---
+++ core-cs/how-to-code-simple-data/1a/bsl-problem-5.rkt
@@ -0,0 +1,18 @@
+; PROBLEM:
+;
+; Based on the two constants provided, write three expressions to determine whether: 
+
+(require 2htdp/image)
+
+(define IMAGE1 (rectangle 10 15 "solid" "red"))
+(define IMAGE2 (rectangle 15 10 "solid" "red"))
+
+; 1) IMAGE1 is taller than IMAGE2
+; 2) IMAGE1 is narrower than IMAGE2
+; 3) IMAGE1 has both the same width AND height as IMAGE2
+
+(> (image-height IMAGE1) (image-height IMAGE2))
+(< (image-width IMAGE1) (image-width IMAGE2))
+
+(and (= (image-width IMAGE1) (image-width IMAGE2))
+     (= (image-height IMAGE1) (image-height IMAGE2)))
